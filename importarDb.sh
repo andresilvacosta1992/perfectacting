8aa9104c590c4dcbed1ff5a9071a76c1eefad4a1
André Costa <andresilvacosta1992@gmail.com>
2024-03-18 10:43:37 -0300

    add importarDb e consegui importar o db

---
+++ importarDb.sh
@@ -0,0 +1,3 @@
+#precisa configurar para cada ambiente/localhost/hospedagem que trabalhar
+
+mysql -u root -pdb32255289 --max_allowed_packet=128M perfectacting_wp < db.sql
--- wp-config.php
+++ wp-config.php
@@ -26,13 +26,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', '23_05_perfectacting' );
+define( 'DB_NAME', 'perfectacting_wp' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'db32255289' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
